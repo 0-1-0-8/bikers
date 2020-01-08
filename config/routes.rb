27a9de738315422527401034eeac7c93efe33227
0-1-0-8 <soccer.0108@icloud.com>
2020-01-08 06:48:11 +0000

    controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'bikes/index'
+  get 'bikes/show'
+  get 'bikes/edit'
+  get 'bikes/update'
+  get 'bikes/destroy'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
