e506287fda2ec91a3f1ca51f012880fef5dd12d0
0-1-0-8 <soccer.0108@icloud.com>
2020-01-26 08:01:24 +0000

    follow

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -3,7 +3,13 @@ class BikesController < ApplicationController
 
   def index
     @bike = Bike.new
-    @bikes = Bike.all
+    @q = Bike.ransack(params[:q])
+    @bikes = @q.result(distinct: true)
+  end
+
+  def search
+    @q = Bike.search(search_params)
+    @bikes = @q.result(distinct: true)
   end
 
   def show
@@ -31,9 +37,12 @@ class BikesController < ApplicationController
   end
   private
   def bike_params
-    params.require(:bike).permit(:text, :image,:user_id, :video)
+    params.require(:bike).permit(:text,:user_id, :video, bike_images_images: [])
   end
   def setbike
     @bike = current_user.bikes.find(params[:id])
+  end
+  def search_params
+    params.require(:q).permit!
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_20_114941) do
+ActiveRecord::Schema.define(version: 2020_01_25_051925) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2020_01_20_114941) do
     t.string "password"
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "bike_images", force: :cascade do |t|
+    t.integer "bike_id"
+    t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "bikes", force: :cascade do |t|
@@ -55,10 +62,9 @@ ActiveRecord::Schema.define(version: 2020_01_20_114941) do
   end
 
   create_table "follows", force: :cascade do |t|
-    t.integer "following"
-    t.integer "follower"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_follows_on_follower_id_and_followed_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
